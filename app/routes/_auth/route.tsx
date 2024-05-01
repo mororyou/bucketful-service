4c78cdbcf90bc8bfa00ab178da712d511ce69542
R.Moroizumi <m.ryousuke0401@icloud.com>
2024-05-02 01:18:55 +0900

    :art: Fix: Auth Page Layout fix.

--- app/routes/_auth/route.tsx
+++ app/routes/_auth/route.tsx
@@ -2,8 +2,10 @@ import { Outlet } from '@remix-run/react';
 
 export default function AuthRouteOutlet() {
   return (
-    <div className="grid min-h-screen items-center justify-center">
-      <Outlet />
+    <div className="grid min-h-screen items-center justify-center gap-6 px-4 md:px-6">
+      <div className="space-y-6 md:min-w-[480px]">
+        <Outlet />
+      </div>
     </div>
   );
 }
