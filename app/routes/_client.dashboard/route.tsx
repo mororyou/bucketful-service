80883928e92ac3b871d15b618274fc1992120032
R.Moroizumi <m.ryousuke0401@icloud.com>
2024-05-03 18:34:51 +0900

    :sparkles: Feat: Outlet AuthUser Context Create

--- app/routes/_client.dashboard/route.tsx
+++ app/routes/_client.dashboard/route.tsx
@@ -1,5 +1,8 @@
 import DashboardContainer from '~app/components/pages/client/dashboard';
+import { useAuthUser } from '../_client/route';
 
 export default function Dashboard() {
+  const user = useAuthUser();
+  console.log(user);
   return <DashboardContainer />;
 }
