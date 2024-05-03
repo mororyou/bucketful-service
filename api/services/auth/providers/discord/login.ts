fe7be3e695c6a7f84e8d9aa98b2f0425e177dfd4
R.Moroizumi <m.ryousuke0401@icloud.com>
2024-05-03 15:57:51 +0900

    :sparkles: Feat: Social Provider Login/Register

---
+++ api/services/auth/providers/discord/login.ts
@@ -0,0 +1,27 @@
+import { AuthorizationError } from 'remix-auth';
+import { DiscordProfile } from 'remix-auth-discord';
+import getUserDataService from '~api/services/produce-public/user/getDataService';
+
+export async function loginDiscordAccount(profile: DiscordProfile) {
+  try {
+    if (!profile.emails) {
+      throw new AuthorizationError('メールアドレスが取得できませんでした。');
+    }
+
+    const user = await getUserDataService({
+      email: profile.emails[0].value,
+      provider: 'discord',
+    });
+
+    if (!user) return null;
+
+    if (user.leave) throw new AuthorizationError('退会済みユーザーです。');
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: _, ...userWithoutPassword } = user;
+
+    return userWithoutPassword;
+  } catch (error) {
+    console.error(error);
+  }
+}
---
+++ api/services/auth/providers/github/login.ts
@@ -0,0 +1,27 @@
+import { AuthorizationError } from 'remix-auth';
+import { GitHubProfile } from 'remix-auth-github';
+import getUserDataService from '~api/services/produce-public/user/getDataService';
+
+export async function loginGithubAccount(profile: GitHubProfile) {
+  try {
+    if (!profile.emails) {
+      throw new AuthorizationError('メールアドレスが取得できませんでした。');
+    }
+
+    const user = await getUserDataService({
+      email: profile.emails[0].value,
+      provider: 'github',
+    });
+
+    if (!user) return null;
+
+    if (user.leave) throw new AuthorizationError('退会済みユーザーです。');
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: _, ...userWithoutPassword } = user;
+
+    return userWithoutPassword;
+  } catch (error) {
+    console.error(error);
+  }
+}
