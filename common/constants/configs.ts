42f93c98e0499bf0220219b4e8f706d49e8445f1
R.Moroizumi <m.ryousuke0401@icloud.com>
2024-05-04 16:47:13 +0900

    :art: Feat: Default image, svg / constant update

--- common/constants/configs.ts
+++ common/constants/configs.ts
@@ -1,5 +1,7 @@
 export const isProd = process.env.PUBLIC_APP_MODE === 'production';
 
+export const APP_NAME = 'Bucketful';
+
 export const successRedirectUrl = '/dashboard';
 
 export const failureRedirectLoginUrl = '/login';
